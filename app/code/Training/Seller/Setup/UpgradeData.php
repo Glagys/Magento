44d9b08503818767be6843d4614b67aabc12763c
Gladys SOLER <soler.gladys@gmail.com>
2017-09-29 09:45:37 +0200

    Training : Seller 8

--- app/code/Training/Seller/Setup/UpgradeData.php
+++ app/code/Training/Seller/Setup/UpgradeData.php
@@ -9,6 +9,9 @@ use Magento\Framework\Setup\UpgradeDataInterface;
 use Magento\Framework\Setup\ModuleContextInterface;
 use Magento\Framework\Setup\ModuleDataSetupInterface;
 use Magento\Eav\Model\Config as EavConfig;
+use Magento\Eav\Setup\EavSetupFactory;
+use Magento\Eav\Setup\EavSetup;
+use Magento\Catalog\Model\Product;
 use Magento\Customer\Setup\CustomerSetupFactory;
 use Magento\Customer\Setup\CustomerSetup;
 use Magento\Customer\Model\Customer;
@@ -26,15 +29,27 @@ class UpgradeData implements UpgradeDataInterface {
     protected $customerSetupFactory;
 
     /**
+     * EAV setup factory
+     *
+     * @var EavSetupFactory
+     */
+    protected $eavSetupFactory;
+
+    /**
      * PHP Constructor
      *
      * @param EavConfig            $eavConfig
      * @param CustomerSetupFactory $customerSetupFactory
+     * @param EavSetupFactory      $eavSetupFactory
+     *
+     * @return InstallData
      */
     public function __construct(EavConfig $eavConfig,
-                                CustomerSetupFactory $customerSetupFactory) {
+                                CustomerSetupFactory $customerSetupFactory,
+                                EavSetupFactory $eavSetupFactory) {
         $this->eavConfig            = $eavConfig;
         $this->customerSetupFactory = $customerSetupFactory;
+        $this->eavSetupFactory      = $eavSetupFactory;
     }
 
     /**
@@ -51,6 +66,10 @@ class UpgradeData implements UpgradeDataInterface {
 
         if (version_compare($context->getVersion(), '1.0.2', '<')) {
             $this->addCustomerAttribute($setup);
+        }
+
+        if (version_compare($context->getVersion(), '1.0.3', '<')) {
+            $this->addProductAttribute($setup);
         }
 
         $setup->endSetup();
@@ -86,6 +105,61 @@ class UpgradeData implements UpgradeDataInterface {
         $attribute = $customerSetup->getEavConfig()->getAttribute('customer', 'training_seller_id');
         $attribute->setData('used_in_forms', ['adminhtml_customer']);
         $attribute->save();
+
+        $this->eavConfig->clear();
+    }
+
+
+    /**
+     * Add the attribute "training_seller_id" on the product model
+     *
+     * @param ModuleDataSetupInterface $setup
+     *
+     * @return void
+     */
+    protected function addProductAttribute(ModuleDataSetupInterface $setup) {
+        /** @var EavSetup $eavSetup */
+        $eavSetup = $this->eavSetupFactory->create(['setup' => $setup]);
+
+        $eavSetup->addAttribute(
+            Product::ENTITY,
+            'training_seller_ids',
+            [
+                'label'                    => 'Training Sellers',
+                'type'                     => 'varchar',
+                'input'                    => 'multiselect',
+                'backend'                  => \Magento\Eav\Model\Entity\Attribute\Backend\ArrayBackend::class,
+                'frontend'                 => '',
+                'class'                    => '',
+                'source'                   => \Training\Seller\Option\Seller::class,
+                'global'                   => \Magento\Catalog\Model\ResourceModel\Eav\Attribute::SCOPE_GLOBAL,
+                'visible'                  => true,
+                'required'                 => false,
+                'user_defined'             => true,
+                'default'                  => '',
+                'searchable'               => false,
+                'filterable'               => false,
+                'comparable'               => true,
+                'visible_on_front'         => true,
+                'used_in_product_listing'  => false,
+                'is_html_allowed_on_front' => true,
+                'unique'                   => false,
+                'apply_to'                 => 'simple,configurable'
+            ]
+        );
+
+        $eavSetup->addAttributeGroup(
+            Product::ENTITY,
+            'bag',
+            'Training'
+        );
+
+        $eavSetup->addAttributeToGroup(
+            Product::ENTITY,
+            'bag',
+            'Training',
+            'training_seller_ids'
+        );
 
         $this->eavConfig->clear();
     }
